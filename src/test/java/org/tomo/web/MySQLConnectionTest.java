60f4591b709ea4862596ef70757f4448639d12bd
hws0205 <hws0205@naer.com>
2016-06-14 10:45:45 +0900

    git test 작성일자 : 2016-06-15 작성자 : 한원섭

---
+++ src/test/java/org/tomo/web/MySQLConnectionTest.java
@@ -0,0 +1,25 @@
+package org.tomo.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySQLConnectionTest {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/tomo";
+	private static final String USER = "testuser";
+	private static final String PW = "1234";
+	
+	@Test
+	public void testConnection() throws Exception{
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			System.out.println(con);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
